999c5b548b325d2f9de5d3a01033b9d0cd5be2c5
Andrew Man Hay Chiu <chiu.andrew.a@edumail.vic.gov.au>
2019-09-23 16:37:22 +1000

    Update r trading tutorial (datacamp).R

--- src/r trading tutorial (datacamp).R
+++ src/r trading tutorial (datacamp).R
@@ -9,11 +9,20 @@ amzn <- get_stock_hist(stock = "amzn")
 
 amzn$timestamp <- ymd(amzn$timestamp)
 
+amzn <- amzn[order(amzn$timestamp),]
 head(amzn)
+
 
 
 # Plot amzn
 plot(amzn$timestamp, amzn$close)
 
-dygraph(amzn$close) %>%
-  dyRangeSelector(dateWindow = c("2013-12-18", "2016-12-30"))
+# Basic Trading Strategy
+# 
+# Moving average
+# Buy when 50-day moving average is below the 200-day moving average
+# Sell when 50-day moving average is above the 200-day moving average
+amzn$sma_200 <- SMA(amzn$close, n = 200)
+amzn$sma_50  <- SMA(amzn$close, n = 50)
+
+head(amzn)
