0b5f52fe81834e0980289611aaa4ae18e8ae4d7e
Andrew Man Hay Chiu <chiu.andrew.a@edumail.vic.gov.au>
2019-10-04 10:53:21 +1000

    migrate twitter sentiments
    Move twitter sentiments page from an old app into Dashboard

--- apps/dashboard/server.R
+++ apps/dashboard/server.R
@@ -3,7 +3,9 @@ library(shiny)
 library(ggplot2)
 library(plotly)
 
-shinyServer(function(input, output) {
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
+shinyServer(function(input, output, session) {
     
     # reactive data for selected stock
     stock_df <- reactive({
@@ -62,6 +64,19 @@ shinyServer(function(input, output) {
                              bgcolor = "transparent"))
       p
 
+    })
+    
+    
+    tweets_df <- reactive({
+      tweets <- searchTwitter(input$stringSearch, n = 25)
+      
+      twListToDF(tweets)
+    })
+    
+    output$searchResults <- renderTable({
+      
+      tweets_df()[, c(1, 11, 12)]
+      
     })
 
 })
