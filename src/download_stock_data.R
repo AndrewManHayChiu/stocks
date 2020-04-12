10ba634ddb143c271a469b732aaff9012ed8e60f
Andrew Man Hay Chiu <amhchiu@gmail.com>
2020-04-12 21:59:37 +1000

    Clean up and simplify scripts

---
+++ src/download_stock_data.R
@@ -0,0 +1,49 @@
+# Download historical data
+# Save data in separate CSV files
+
+# Data can be updated with new data using the updateData.R script
+
+library(zoo)
+
+source("src/functions.R")
+source("src/tickers.R")
+
+path <- "data"
+
+# Download daily data
+# 5 at a time with free account from AlphaVantage
+for (i in 1) {
+  
+  tryCatch({
+    print(tickers[i])
+    
+    data <- get_alphavantage(stock = tickers[i], outputsize = "full")
+    
+    filePath <- paste0(path, "/daily/",tickers[i], ".csv")
+    
+    write.zoo(data, filePath, sep = ",", row.names = F)
+  }, 
+  
+  error = function(e) {cat("ERROR :", conditionMessage(e), "\n")}
+  )
+  
+}
+
+# Download Intraday data
+
+for (i in 1) {
+  
+  tryCatch({
+    print(tickers[i])
+    
+    data <- get_alphavantage(stock = tickers[1], series = "TIME_SERIES_INTRADAY", outputsize = "full", interval = "5min")
+    
+    filePath <- paste0(path, "/intraday/", tickers[i], ".csv")
+    
+    write.zoo(data, filePath, sep = ",", row.names = F)
+  }, 
+  
+  error = function(e) {cat("ERROR :", conditionMessage(e), "\n")}
+  )
+  
+}
---
+++ src/tickers.R
@@ -0,0 +1,13 @@
+# List of tickers
+# October 2019
+
+
+# tickers <- c("BHP", "QAN", "TLS", "CSL", "WOW", "MQG", "CBA", "NAB", "WES",
+#              "NEC", "BKW", "NXT", "CTW", "BKL", "WEB",
+#              "IDX", "VRL", "MYR", "AKP", "KMD", "MVF",
+#              "AVZ", "AYS", "4DS", "TWD", "CCV", "SSG")
+
+tickers <- c("PGL", "SCU", "CDY", "AMA", "CDY")
+
+# Make sure there are no duplicates
+tickers <- unique(tickers)
